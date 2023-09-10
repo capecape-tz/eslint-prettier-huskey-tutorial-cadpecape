20e7937befcec9f779b5294a7e51a2c738570169
capecape-tz <capecape.tzgmail.com>
2023-09-10 15:25:10 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('aaa');
+
+const boolean = true;
+if (boolean) {
+  const aa = 'aa';
+}
+
+const eee = 12345;
+const asdfa = 1111;
+
+const camelCase = 123;
+
+const { test1, test2 } = { eee, camelCase };
